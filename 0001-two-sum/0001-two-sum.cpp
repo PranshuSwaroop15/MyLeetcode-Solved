15b01955f4634a5be2e6ea8822c3376be3853aa9
PranshuSwaroop15 <107299963+PranshuSwaroop15@users.noreply.github.com>
2023-01-19 00:02:19 +0530

    Time: 561 ms (28.41%), Space: 10.3 MB (73.91%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        vector<int>ans;
+        
+        
+        for(int i=0;i<nums.size();i++){
+            
+            for(int j=i+1;j<nums.size();j++){
+                
+                if(nums[i]+nums[j]==target){
+                    
+                    ans.push_back(i);
+
+                    ans.push_back(j);
+                }
+                    
+            }
+        }
+        
+        // int i=0;
+        // int j=nums.size()-1;
+        
+        
+        // while(i<j){
+            
+        //     if(nums[i]+nums[j]==target){
+                
+        //         ans.push_back(i);
+        //         ans.push_back(j);
+        //         i++;
+        //     }
+            
+        //     j--;
+            
+        // }
+        
+        return ans;
+        
+    }
+};
