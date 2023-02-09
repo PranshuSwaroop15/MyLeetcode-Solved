e66852b08344f673d79d634564323de63d9f1fe3
PranshuSwaroop15 <107299963+PranshuSwaroop15@users.noreply.github.com>
2023-02-09 22:15:22 +0530

    Time: 17 ms (31.47%), Space: 16 MB (96.31%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -8,20 +8,23 @@ public:
         
         int n=(int)nums.size();
         
-        while(pt1<n &&pt2<n){
+        while(pt1<n && pt2<n){
             
             
             if(nums[pt2]%2==0){
+                
                 
                 swap(nums[pt1],nums[pt2]);
                 
                 pt1++;
             }
             
-            
             pt2++;
+            
         }
         
+        
         return nums;
+        
     }
 };
