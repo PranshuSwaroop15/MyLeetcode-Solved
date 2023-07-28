d04dedc2ea5c7ed0921885d61e9ce535563b9d3c
PranshuSwaroop15 <107299963+PranshuSwaroop15@users.noreply.github.com>
2023-07-28 23:15:26 +0530

    Time: 5 ms (42.47%), Space: 6.8 MB (97.05%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string decodeMessage(string key, string m,int k=0) {
+    vector<char> map(26);
+    
+    //create mapping
+    for(auto i:key){
+        // current is not a space and is not found before
+        if(i!=32 and !map[i-'a']) map[i-'a']= (k+'a') , k++;
+	}
+    
+    //then update characters inplace in message string according to mapping except for spaces
+    for(int i=0;i<m.size();i++) if(m[i]!=32) m[i]= map[m[i]-'a'];
+    return m;
+}
+};
