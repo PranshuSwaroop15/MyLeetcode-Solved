86edb6ce982c2c99c194d69a899f0e5418c58c6e
PranshuSwaroop15 <107299963+PranshuSwaroop15@users.noreply.github.com>
2023-07-28 22:57:21 +0530

    Time: 139 ms (88.23%), Space: 24 MB (86.04%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,59 +1,53 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
-        vector<vector<int>>res;
-        
-        sort(nums.begin(),nums.end());
-        
-        int n=(int)nums.size();
-        
-        
-        for(int i=0;i<n;i++){
-            
-            int target=-nums[i];
-            
-            int front=i+1;
-            
-            int back=n-1;
-            
-            
-            while(front<back){
-                
-                int sum=nums[front]+nums[back];
-                
-                if(sum<target){
-                    front++;
-                }
-                else if(sum>target)
-                {
-                    back--;
-                }
-                else{
-                    
-                    vector<int>triplet={nums[i],nums[front],nums[back]};
-                    
-                    res.push_back(triplet);
-                    
-                    //             checking for duplication of number at index 1
-                    while(front<back && nums[front]==triplet[1]){
-                        front++;
-                    }
-//                   checking for duplication of number at index 2
-                    while(front<back && nums[back]==triplet[2]){
-                         back--;
-                     }
-                }
+        vector<vector<int>> res;
+      
+      // size of the array nums
+      int total = nums.size(); 
+      
+      // sort the numbers
+      sort(nums.begin(), nums.end());
+      
+      // fix the first number of the triplet
+      for (int firstNumIdx = 0; firstNumIdx < total; ++firstNumIdx) {
+        int firstNum = nums[firstNumIdx];
+        // find pairs with sum = -firstNum in the right
+        int start = firstNumIdx + 1;
+        int end = total - 1;
+        while (start < end) {
+          // consider the current pair sum
+          int current = nums[start] + nums[end];
+          if (current < -firstNum) {
+            // shift the start pointer to the right
+            ++start;
+          } else if (current > -firstNum) {
+            // shift the end pointer to the left
+            --end;
+          } else {
+            // add to the result
+            res.push_back({firstNum, nums[start], nums[end]});
+            int oldStart = start;
+            int oldEnd = end;
+            // shift the start till it matches the old value
+            while (start < end && nums[start] == nums[oldStart]) {
+              ++start;
             }
-
-//         checking for duplication of number at index 0
-            while(i+1<n && nums[i+1]==nums[i]){
-                 
-                 i++;
+            // shift the end till it matches the old value
+            while (end > start && nums[end] == nums[oldEnd]) {
+              --end;
+            }
+            // the above two while loops ensure that both start/end
+            // get shifted atleast once
+          }
+          // avoid duplicates
+          while (firstNumIdx + 1 < total 
+                 && nums[firstNumIdx + 1] == nums[firstNumIdx]) {
+            ++firstNumIdx;
+          }
         }
-            
-        }
-       
-        return res;
+      }
+      
+      return res;
     }
 };
